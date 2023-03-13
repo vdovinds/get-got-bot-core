d9347b2afe6c3c4efa99a575f64246814926b25a
Dmitry Vdovin <dmitry.vdovin@raiffeisen.ru>
2023-03-13 15:55:01 +0300

    added github actions

--- handler.py
+++ handler.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def handler(event, context):
+    httpMethod = event["httpMethod"]
+    params = event["queryStringParameters"]
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return {
+        'statusCode': 200,
+        #'body': json.dumps(params),
+        'body': "ok",
+    }
