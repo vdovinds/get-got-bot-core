b49f93a6556a2ec8408f11feaf43d032686ea6c8
Dmitry Vdovin <dmitry.vdovin@raiffeisen.ru>
2023-03-14 10:21:24 +0300

    one more test added firebase

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import os
 import json
+import ast
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
 
 private_key_id = os.environ.get('FB_PRIVATE_KEY_ID')
-private_key = os.environ.get('FB_PRIVATE_KEY')
+private_key = ast.literal_eval(os.environ.get('FB_PRIVATE_KEY'))
 
 with open("key.json") as json_file:
     key_file = json.load(json_file)
     json_file.close()
 
-with open("/tmp/key.json", "w", encoding='utf-8') as json_file:
+with open("/tmp/key.json", "w") as json_file:
     key_file['private_key_id'] = private_key_id
     key_file['private_key'] = private_key
     json_object = json.dumps(key_file, ensure_ascii=False, indent=4)
-    print(json_object)
     json_file.write(json_object)
     json_file.close()
 
